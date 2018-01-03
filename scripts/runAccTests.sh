8adfcc7551e43a05c08496338a8e8bbf335e057b
Manuel Vogel <vogel@stocard.de>
2018-01-03 14:30:27 +0100

    Simplified acc tests process into a single script.

---
+++ scripts/runAccTests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+log() {
+  echo ""
+  echo "##################################"
+  echo "-------> $1"
+  echo "##################################"
+}
+
+setup() {
+  export DOCKER_REGISTRY_ADDRESS="127.0.0.1:5000"
+  export DOCKER_REGISTRY_USER="testuser"
+  export DOCKER_REGISTRY_PASS="testpwd"
+  export DOCKER_PRIVATE_IMAGE="127.0.0.1:5000/my-private-service"
+  sh scripts/testing/setup_private_registry.sh
+}
+
+run() {
+  # Run the acc test suite
+  make testacc
+  # for a single test
+  # TF_ACC=1 go test -v -timeout 30s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerRegistryImage_private$
+}
+
+cleanup() {
+  docker stop private_registry
+  unset DOCKER_REGISTRY_ADDRESS DOCKER_REGISTRY_USER DOCKER_REGISTRY_PASS DOCKER_PRIVATE_IMAGE
+  rm -f scripts/testing/auth/htpasswd
+  rm -f scripts/testing/certs/registry_auth.*
+}
+
+## main
+log "setup" && setup 
+log "run" && run
+log "cleanup" && cleanup
