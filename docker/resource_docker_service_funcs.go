1ef47d13bf07f956688c0d90a444ba43063db8fb
Manuel Vogel <vogel@stocard.de>
2018-01-11 10:02:41 +0100

    Fixed decrease of service replicas.

--- docker/resource_docker_service_funcs.go
+++ docker/resource_docker_service_funcs.go
@@ -66,7 +66,8 @@ func resourceDockerServiceCreate(d *schema.ResourceData, meta interface{}) error
 
 	configIDs := extractSetProperty(d, "configs", "config_id")
 	secretIDs := extractSetProperty(d, "secrets", "secret_id")
-	if err := areAtLeastNContainersUp(d.Get("name").(string), d.Get("image").(string), service.ID, configIDs, secretIDs, d.Get("replicas").(int), client); err != nil {
+	oldN, newN := d.GetChange("replicas")
+	if err := areAtLeastNContainersUp(d.Get("name").(string), d.Get("image").(string), service.ID, configIDs, secretIDs, oldN.(int), newN.(int), client); err != nil {
 		return err
 	}
 
@@ -131,7 +132,8 @@ func resourceDockerServiceUpdate(d *schema.ResourceData, meta interface{}) error
 
 	configIDs := extractSetProperty(d, "configs", "config_id")
 	secretIDs := extractSetProperty(d, "secrets", "secret_id")
-	if err := areAtLeastNContainersUp(d.Get("name").(string), d.Get("image").(string), d.Id(), configIDs, secretIDs, d.Get("replicas").(int), client); err != nil {
+	oldN, newN := d.GetChange("replicas")
+	if err := areAtLeastNContainersUp(d.Get("name").(string), d.Get("image").(string), d.Id(), configIDs, secretIDs, oldN.(int), newN.(int), client); err != nil {
 		return err
 	}
 
@@ -583,78 +585,81 @@ func extractSetProperty(d *schema.ResourceData, setKey string, key string) []str
 	return properties
 }
 
-func areAtLeastNContainersUp(serviceName string, image string, serviceID string, configIDs []string, secretIDs []string, n int, client *dc.Client) error {
-	// config
-	loops := 60
-	sleepTime := 1000 * time.Millisecond
-	maxErrorCount := 3
+func areAtLeastNContainersUp(serviceName string, image string, serviceID string, configIDs []string, secretIDs []string, oldN int, newN int, client *dc.Client) error {
+	// handle the special case of decreasing the replicas
+	if newN >= oldN {
+		// config
+		loops := 60
+		sleepTime := 1000 * time.Millisecond
+		maxErrorCount := 3
 
-	// == 1: get at least one task for the given service name
-	taskIDs := make([]string, 0)
-	filter := make(map[string][]string)
-	filter["service"] = []string{serviceName}
+		// == 1: get at least one task for the given service name
+		taskIDs := make([]string, 0)
+		filter := make(map[string][]string)
+		filter["service"] = []string{serviceName}
 
-	for i := 1; i <= loops; i++ {
-		tasks, err := client.ListTasks(dc.ListTasksOptions{
-			Filters: filter,
-		})
-		if err != nil {
-			return err
-		}
-		amountOfTasksWithImageConfigAndSecret := getAmountOfTasksWithImageConfigAndSecret(tasks, image, configIDs, secretIDs)
-		if amountOfTasksWithImageConfigAndSecret >= n {
-			for _, task := range tasks {
-				taskIDs = append(taskIDs, task.ID)
-			}
-			log.Printf("[INFO] Got at least %d running task(s) for service '%s' and image '%s' after %d seconds", n, serviceName, image, i)
-			break
-		}
-		log.Printf("[INFO] Service '%s' task loop: %02d/%d for amount of registered tasks %02d/%d", serviceName, i, loops, amountOfTasksWithImageConfigAndSecret, n)
-		time.Sleep(sleepTime)
-	}
-
-	// no running task found -> deleting service
-	if len(taskIDs) == 0 {
-		log.Printf("[INFO] Found no running task for service '%s' after %d seconds", serviceName, loops)
-		deleteService(serviceID, client)
-		return fmt.Errorf("[INFO] Deleted service %s", serviceName)
-	}
-
-	// == 2: inspect that n tasks are up
-	for _, taskID := range taskIDs {
-		errorCount := 0
 		for i := 1; i <= loops; i++ {
-			task, err := client.InspectTask(taskID)
-			// handle special case of decreasing amount of replicas
-			// then the taskID may not exist any more at this point
+			tasks, err := client.ListTasks(dc.ListTasksOptions{
+				Filters: filter,
+			})
 			if err != nil {
-				if !strings.Contains(err.Error(), "No such task") {
-					return err
+				return err
+			}
+			amountOfTasksWithImageConfigAndSecret := getAmountOfTasksWithImageConfigAndSecret(tasks, image, configIDs, secretIDs)
+			if amountOfTasksWithImageConfigAndSecret >= newN {
+				for _, task := range tasks {
+					taskIDs = append(taskIDs, task.ID)
 				}
-				// TODO here: seg fault somtimes
-				log.Printf("[INFO] --> new error: %s", err.Error())
+				log.Printf("[INFO] Got at least %d running task(s) for service '%s' and image '%s' after %d seconds", newN, serviceName, image, i)
+				break
 			}
-			log.Printf("[INFO] Inspecting task with ID '%s' in loop %02d/%d to be in state: [%s]->[%s]", taskID, i, loops, task.Status.State, task.DesiredState)
-			if task.DesiredState == task.Status.State {
-				log.Printf("[INFO] Task '%s' containerID '%s' is in desired state '%s'", taskID, task.Status.ContainerStatus.ContainerID, task.DesiredState)
-				break // success here, next task
-			}
+			log.Printf("[INFO] Service '%s' task loop: %02d/%d for amount of registered tasks %02d/%d", serviceName, i, loops, amountOfTasksWithImageConfigAndSecret, newN)
+			time.Sleep(sleepTime)
+		}
 
-			if task.Status.State == swarm.TaskStateFailed ||
-				task.Status.State == swarm.TaskStateRejected ||
-				task.Status.State == swarm.TaskStateShutdown {
-				errorCount++
-				log.Printf("[INFO] Task '%s' containerID '%s' is in error state '%s' for %d/%d", taskID, task.Status.ContainerStatus.ContainerID, task.Status.State, errorCount, maxErrorCount)
-				if errorCount >= maxErrorCount {
-					log.Printf("[INFO] Task '%s' for service '%s' in state '%s' after %d errors. Deleting service!", taskID, serviceName, task.Status.State, maxErrorCount)
-					deleteService(serviceID, client)
-					return fmt.Errorf("[INFO] Deleted service '%s'", serviceName)
+		// no running task found -> deleting service
+		if len(taskIDs) == 0 {
+			log.Printf("[INFO] Found no running task for service '%s' after %d seconds", serviceName, loops)
+			deleteService(serviceID, client)
+			return fmt.Errorf("[INFO] Deleted service %s", serviceName)
+		}
+
+		// == 2: inspect that n tasks are up
+		for _, taskID := range taskIDs {
+			errorCount := 0
+			for i := 1; i <= loops; i++ {
+				task, err := client.InspectTask(taskID)
+				// handle special case of decreasing amount of replicas
+				// then the taskID may not exist any more at this point
+				if err != nil {
+					if strings.Contains(err.Error(), "No such task") {
+						log.Printf("[INFO] No such task for taskID '%s'. Going to next one", taskID)
+						break // go to next task
+					}
+					return err // all other errors should be reported
 				}
+				log.Printf("[INFO] Inspecting task with ID '%s' in loop %02d/%d to be in state: [%s]->[%s]", taskID, i, loops, task.Status.State, task.DesiredState)
+				if task.DesiredState == task.Status.State {
+					log.Printf("[INFO] Task '%s' containerID '%s' is in desired state '%s'", taskID, task.Status.ContainerStatus.ContainerID, task.DesiredState)
+					break // success here, next task
+				}
+
+				if task.Status.State == swarm.TaskStateFailed ||
+					task.Status.State == swarm.TaskStateRejected ||
+					task.Status.State == swarm.TaskStateShutdown {
+					errorCount++
+					log.Printf("[INFO] Task '%s' containerID '%s' is in error state '%s' for %d/%d", taskID, task.Status.ContainerStatus.ContainerID, task.Status.State, errorCount, maxErrorCount)
+					if errorCount >= maxErrorCount {
+						log.Printf("[INFO] Task '%s' for service '%s' in state '%s' after %d errors. Deleting service!", taskID, serviceName, task.Status.State, maxErrorCount)
+						deleteService(serviceID, client)
+						return fmt.Errorf("[INFO] Deleted service '%s'", serviceName)
+					}
+				}
+				time.Sleep(sleepTime) // sleep between inspecting tasks
 			}
-			time.Sleep(sleepTime) // sleep between inspecting tasks
 		}
 	}
 
-	log.Printf("[INFO] >> For service '%s' and image '%s' desired '%d' replica(s) are up!", serviceName, image, n)
+	log.Printf("[INFO] For service '%s' and image '%s' desired '%d -> %d' replica(s) are up!", serviceName, image, oldN, newN)
 	return nil
 }
