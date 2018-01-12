61c197ae6e1180856b2fc61e31b67553347a1de0
Manuel Vogel <vogel@stocard.de>
2018-01-12 16:06:46 +0100

    Added debug info for cleanup tests.

--- scripts/runAccTests.sh
+++ scripts/runAccTests.sh
@@ -20,9 +20,9 @@ run() {
   # Run the acc test suite
   #make testacc
   # for a single test
-  TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateImage$
-  TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateConfigForMultipleServices$
-  TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateConfigAndDecreaseReplicas$
+  #TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateImage$
+  #TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateConfigForMultipleServices$
+  #TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateConfigAndDecreaseReplicas$
   TF_LOG=INFO TF_ACC=1 go test -v -timeout 240s github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_updateConfigReplicasImageAndHealthIncreaseAndDecreaseReplicas$
   
   # keep the return for the scripts to fail and clean properly
@@ -31,10 +31,14 @@ run() {
 
 cleanup() {
   unset DOCKER_REGISTRY_ADDRESS DOCKER_REGISTRY_USER DOCKER_REGISTRY_PASS DOCKER_PRIVATE_IMAGE
+  echo "### unsetted env ###"
   rm -f scripts/testing/auth/htpasswd
   rm -f scripts/testing/certs/registry_auth.*
+  echo "### removed auth and certs ###"
   docker stop private_registry
+  echo "### stopped private registry ###"
   docker rmi -f $(docker images -aq 127.0.0.1:5000/my-private-service)
+  echo "### removed my-private-service images ###"
   # consider running this manually to clean up the
   # updateabe configs and secrets
   #docker config rm $(docker config ls -q)
