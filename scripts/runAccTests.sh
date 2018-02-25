5746df69a19c8693396b1e14ae81ca54d1cb21b9
Manuel Vogel <vogel@stocard.de>
2018-02-25 23:17:48 +0100

    Fixed test setup

--- scripts/runAccTests.sh
+++ scripts/runAccTests.sh
@@ -21,8 +21,7 @@ run() {
   TF_ACC=1 go test ./docker -v -timeout 120m
   
   # for a single test
-  #TF_LOG=INFO TF_ACC=1 go test -v github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_full$ -timeout 360s
-  
+  #TF_LOG=INFO TF_ACC=1 go test -v github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_basic$ -timeout 360s
   # keep the return for the scripts to fail and clean properly
   return $?
 }
@@ -30,28 +29,25 @@ run() {
 cleanup() {
   unset DOCKER_REGISTRY_ADDRESS DOCKER_REGISTRY_USER DOCKER_REGISTRY_PASS DOCKER_PRIVATE_IMAGE
   echo "### unsetted env ###"
+  docker stop private_registry
+  echo "### stopped private registry ###"
   rm -f scripts/testing/auth/htpasswd
   rm -f scripts/testing/certs/registry_auth.*
   echo "### removed auth and certs ###"
-  docker stop private_registry
-  echo "### stopped private registry ###"
+  docker rm -f $(docker container ls --filter=name=service- -aq)
+  echo "### removed stopped containers ###"
+  docker config rm $(docker config ls --filter=name=myconfig- -q)
+  docker secret rm $(docker secret ls --filter=name=mysecret- -q)
+  echo "### configs and secrets ###"
+  
+  # fails due to https://github.com/moby/moby/issues/32620 because sometimes there a still running containers of 
+  # removed services
   docker rmi -f $(docker images -aq 127.0.0.1:5000/my-private-service)
   echo "### removed my-private-service images ###"
-  # consider running this manually to clean up the
-  # updateabe configs and secrets
-  # docker config rm $(docker config ls -q)
-  # docker secret rm $(docker secret ls -q)
 }
 
 ## main
 log "setup" && setup 
 log "run" && run && echo $?
-if [ $? -ne 0 ]; then
-  log "cleanup" && cleanup 
-  exit 1
-fi
-# we only clean on local envs. travis fails from time to time there
-# cuz it cannot remove the images
-if [ "$TRAVIS" != "true" ]; then
-  log "cleanup" && cleanup
-fi
+# travis fails from time to time there
+[ "$TRAVIS" != "true" ]; log "cleanup" && cleanup
