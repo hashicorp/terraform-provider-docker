af08d2413eb5e6b184404e984e903f7e12270c2e
Nemo <me@captnemo.in>
2018-06-01 18:36:16 +0530

    Migrates docker_volume (#71)
    migrate docker_volume outline

--- docker/resource_docker_volume_test.go
+++ docker/resource_docker_volume_test.go
@@ -1,67 +1,64 @@
 package docker
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	dc "github.com/docker/docker/client"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"testing"
+)
 
-// 	dc "github.com/fsouza/go-dockerclient"
-// 	"github.com/hashicorp/terraform/helper/resource"
-// 	"github.com/hashicorp/terraform/terraform"
-// )
+func TestAccDockerVolume_basic(t *testing.T) {
+	var v types.Volume
 
-// func TestAccDockerVolume_basic(t *testing.T) {
-// 	var v dc.Volume
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccDockerVolumeConfig,
+				Check: resource.ComposeTestCheckFunc(
+					checkDockerVolume("docker_volume.foo", &v),
+					resource.TestCheckResourceAttr("docker_volume.foo", "id", "testAccDockerVolume_basic"),
+					resource.TestCheckResourceAttr("docker_volume.foo", "name", "testAccDockerVolume_basic"),
+				),
+			},
+		},
+	})
+}
 
-// 	resource.Test(t, resource.TestCase{
-// 		PreCheck:  func() { testAccPreCheck(t) },
-// 		Providers: testAccProviders,
-// 		Steps: []resource.TestStep{
-// 			resource.TestStep{
-// 				Config: testAccDockerVolumeConfig,
-// 				Check: resource.ComposeTestCheckFunc(
-// 					checkDockerVolume("docker_volume.foo", &v),
-// 					resource.TestCheckResourceAttr("docker_volume.foo", "id", "testAccDockerVolume_basic"),
-// 					resource.TestCheckResourceAttr("docker_volume.foo", "name", "testAccDockerVolume_basic"),
-// 				),
-// 			},
-// 		},
-// 	})
-// }
+func checkDockerVolume(n string, volume *types.Volume) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
 
-// func checkDockerVolume(n string, volume *dc.Volume) resource.TestCheckFunc {
-// 	return func(s *terraform.State) error {
-// 		rs, ok := s.RootModule().Resources[n]
-// 		if !ok {
-// 			return fmt.Errorf("Not found: %s", n)
-// 		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
 
-// 		if rs.Primary.ID == "" {
-// 			return fmt.Errorf("No ID is set")
-// 		}
+		ctx := context.Background()
+		client := testAccProvider.Meta().(*ProviderConfig).DockerClient
+		v, err := client.VolumeInspect(ctx, n)
+		// volumes, err := client.VolumeList(ctx, filters.Args{})
+		if err != nil {
+			if dc.IsErrNotFound(err) {
+				return fmt.Errorf("Volume not found: %s", rs.Primary.ID)
+			}
+			return err
+		}
 
-// 		client := testAccProvider.Meta().(*ProviderConfig).DockerClient
-// 		volumes, err := client.ListVolumes(dc.ListVolumesOptions{})
-// 		if err != nil {
-// 			return err
-// 		}
+		*volume = v
 
-// 		for _, v := range volumes {
-// 			if v.Name == rs.Primary.ID {
-// 				inspected, err := client.InspectVolume(v.Name)
-// 				if err != nil {
-// 					return fmt.Errorf("Volume could not be inspected: %s", err)
-// 				}
-// 				*volume = *inspected
-// 				return nil
-// 			}
-// 		}
+		return nil
+	}
+}
 
-// 		return fmt.Errorf("Volume not found: %s", rs.Primary.ID)
-// 	}
-// }
-
-// const testAccDockerVolumeConfig = `
-// resource "docker_volume" "foo" {
-// 	name = "testAccDockerVolume_basic"
-// }
-// `
+const testAccDockerVolumeConfig = `
+resource "docker_volume" "foo" {
+	name = "testAccDockerVolume_basic"
+}
+`
