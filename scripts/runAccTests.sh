4e4bd405e8396149af67d6d7d14fd13c0ca2091c
Manuel Vogel <vogel@stocard.de>
2018-10-15 22:09:45 +0200

    run only port tests

--- scripts/runAccTests.sh
+++ scripts/runAccTests.sh
@@ -13,14 +13,15 @@ setup() {
   export DOCKER_REGISTRY_USER="testuser"
   export DOCKER_REGISTRY_PASS="testpwd"
   export DOCKER_PRIVATE_IMAGE="127.0.0.1:15000/tftest-service:v1"
-  sh "$(pwd)"/scripts/testing/setup_private_registry.sh
+  #sh "$(pwd)"/scripts/testing/setup_private_registry.sh
 }
 
 run() {
-  TF_ACC=1 go test ./docker -v -timeout 120m
+  #TF_ACC=1 go test ./docker -v -timeout 120m
   
   # for a single test comment the previous line and uncomment the next line
-  #TF_LOG=INFO TF_ACC=1 go test -v github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerService_full$ -timeout 360s
+  TF_LOG=INFO TF_ACC=1 go test -v github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerContainer_port$ -timeout 360s
+  TF_LOG=INFO TF_ACC=1 go test -v github.com/terraform-providers/terraform-provider-docker/docker -run ^TestAccDockerContainer_port_internal$ -timeout 360s
   
   # keep the return value for the scripts to fail and clean properly
   return $?
