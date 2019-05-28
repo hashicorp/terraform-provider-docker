92f3a0f3da058311b141273be2e9140b76e299fc
Manuel Vogel <mavogel@posteo.de>
2019-05-28 21:00:11 +0200

    reactivates full build in script and travis

--- scripts/runAccTests.sh
+++ scripts/runAccTests.sh
@@ -49,10 +49,10 @@ setup() {
 
 run() {
   go clean -testcache
-  #TF_ACC=1 go test ./docker -v -timeout 120m
+  TF_ACC=1 go test ./docker -v -timeout 120m
   
   # for a single test comment the previous line and uncomment the next line
-  TF_LOG=DEBUG TF_ACC=1 go test -v ./docker -run ^TestAccDockerService_updateMultiplePropertiesConverge$ -timeout 360s
+  #TF_LOG=DEBUG TF_ACC=1 go test -v ./docker -run ^TestAccDockerService_updateMultiplePropertiesConverge$ -timeout 360s
   
   # keep the return value for the scripts to fail and clean properly
   return $?
