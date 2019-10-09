999ede5db509eb85edc414b79ca4446395dd50f2
Manuel Vogel <mavogel@posteo.de>
2019-10-09 21:49:01 +0200

    docs: secret for next major

--- docker/resource_docker_secret.go
+++ docker/resource_docker_secret.go
@@ -79,6 +79,7 @@ func resourceDockerSecretRead(d *schema.ResourceData, meta interface{}) error {
 	d.SetId(secret.ID)
 	d.Set("name", secret.Spec.Name)
 	// Note mavogel: secret data is not exposed via the API
+	// TODO next major if we do not explicitly do not store it in the state we could import it, but BC
 	// d.Set("data", base64.StdEncoding.EncodeToString(secret.Spec.Data))
 	return nil
 }
