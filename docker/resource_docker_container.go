efd1b59635e89c0ca9c8cf2e5c599bc5413854b7
Nolan Woods <nolan_w@sfu.ca>
2020-11-12 18:00:45 -0800

    Fix schema type

--- docker/resource_docker_container.go
+++ docker/resource_docker_container.go
@@ -227,7 +227,7 @@ func resourceDockerContainer() *schema.Resource {
 				Type:        schema.TypeSet,
 				Optional:    true,
 				ForceNew:    true,
-				Elem:        &schema.Schema{Elem: schema.TypeString},
+				Elem:        &schema.Schema{Type: schema.TypeString},
 				Description: "List of string values to customize labels for MLS systems, such as SELinux. See https://docs.docker.com/engine/reference/run/#security-configuration",
 				Set:         schema.HashString,
 			},
